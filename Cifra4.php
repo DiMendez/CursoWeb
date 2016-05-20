f7b3cfa3bfd7a48b81ef24da149723eb538147ef
DiMendez <dianamendezavalos@gmail.com>
2016-05-20 00:14:24 -0500

    Incluye descifrados

---
+++ Cifra4.php
@@ -0,0 +1,40 @@
+<?php
+	if(isset($_POST["num"]) && isset($_POST["lla"]))
+		{
+			$lla=$_POST["lla"];
+			$cad=str_split($_POST["num"]);
+			$priv='Dim';
+			$n=strlen($lla);
+			$m=count($cad);
+			if(isset($_POST["check"]))
+			{
+				for($i=0;$i<$m;$i++)
+				{
+					$car=$cad[$i];
+					$a=ord($car);
+					$c=$a+($n-strlen($priv));
+					$d=utf8_encode($c);
+					$b=chr($d);
+					$cad[$i]=$b;
+				}
+				foreach($cad as $e)
+					echo $e;
+			}
+			else
+			{
+				for($i=0;$i<$m;$i++)
+				{
+					$car=$cad[$i];
+					$a=ord($car);
+					$c=$a-($n-strlen($priv));
+					$b=chr($c);
+					$cad[$i]=$b;
+				}
+				foreach($cad as $e)
+					echo $e;
+			}
+			
+		}
+		else
+			echo'Primero mete la cadena y la llave';
+?>
